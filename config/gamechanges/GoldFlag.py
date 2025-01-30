c12f6bfac8df9238a95f6f0270db87c52f440e71
gopnuk3000 <apex54311@gmail.com>
2025-01-30 20:38:07 +0300

    Update GoldFlag.py

--- config/gamechanges/GoldFlag.py
+++ config/gamechanges/GoldFlag.py
@@ -1,4 +1,15 @@
 class GoldFlag:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def used(self, x, y):
+        a = []
+        a.append((x, y))
+        for i in range(self.width):
+            for j in range(self.height):
+                if x != i and y != j:
+                    pass
+                else:
+                    a.append((i, j))
+        return a
