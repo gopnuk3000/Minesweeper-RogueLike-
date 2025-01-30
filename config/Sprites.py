5f8e5c70acbce224cc9995b9406161a591ee2edb
gopnuk3000 <apex54311@gmail.com>
2025-01-30 19:01:51 +0300

    Update Sprites.py

--- config/Sprites.py
+++ config/Sprites.py
@@ -1,21 +1,5 @@
 import pygame
-import os
-import sys
-
-def load_image(name, color_key=None):
-    fullname = os.path.join(name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if color_key is not None:
-        image = image.convert()
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
-    return image
+from GameBoard import load_image
 
 
 class MineImage(pygame.sprite.Sprite):
